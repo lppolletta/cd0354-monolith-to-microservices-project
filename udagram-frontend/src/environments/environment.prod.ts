6ea836d03bd1843b6bd5b92aabd3de83f8b97ca6
Lou Polletta <loupolletta@outlook.com>
2022-05-21 13:03:25 -0400

    added k8s

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a4d74c5903f974e50a48135a58415571-2003928766.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
